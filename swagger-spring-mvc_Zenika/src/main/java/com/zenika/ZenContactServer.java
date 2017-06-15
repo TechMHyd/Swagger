bee508a7eb5eb308b2362e843676c9a80b412759
Tech Mahindra ADMS Competency <ravikiran.mallidi@techmahindra.com>
2017-06-15 15:24:13 +0530

    Add files via upload

---
+++ swagger-spring-mvc_Zenika/src/main/java/com/zenika/ZenContactServer.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.zenika;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+public class ZenContactServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		int port = 8090;
+		String contextPath = "zencontact";
+		Server server = new Server(port);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/"+contextPath);
+        server.setHandler(context);
+        
+        WebApplicationContext appContext = ConfigurationUtils.createXmlConfiguration(context.getServletContext());
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(appContext);
+        context.addServlet(new ServletHolder(dispatcherServlet), "/*");
+        
+        server.start();
+        
+        System.out.println("Server started");
+        System.out.printf("REST API documentation: http://localhost:%s/%s/api-docs %n",port,contextPath);
+        System.out.printf("REST API documentation UI: http://localhost:%s/%s/swagger-ui/index.html",port,contextPath);
+        server.join();
+
+	}
+
+}
